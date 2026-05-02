4babd361af6b6a7df8684c784977f2c1e4bc3701
RavexCode <rd77extragd@gmail.com>
2026-05-02 02:10:02 -0600

    Add files via upload

---
+++ src/content/projects.ts
@@ -0,0 +1,21 @@
+export const projects = [
+  {
+    image: "https://raw.githubusercontent.com/RDev00/list-app/refs/heads/web/public/favicon.ico",
+    title: "CloudBook",
+    description: "La principal función de esta aplicación es brindar un bloc de notas para los usuarios con el contenido de estas mismas encriptados de manera completamente segura.",
+    languajes: [ "JavaScript", "Dart", "SQL" ],
+    frameworks: [ "NextJS", "Flutter" ],
+    applications: [ "Vercel", "Supabase" ],
+    displayed_at: "https://cloudbook.ravexcode.com/",
+    repository: "https://github.com/RDev00/list-app"
+  },
+  {
+    image: "https://raw.githubusercontent.com/RDev00/konfia/refs/heads/main/public/icons/appicon.png",
+    title: "Konfia",
+    description: "Un gestor de finanzas en base a creditos. Enfocado a tiendas que solían usar tablas para guardar los clientes que le fían.",
+    languajes: [ "JavaScript", "SQL" ],
+    frameworks: [ "ViteJS", "ExpressJS", "NodeJS" ],
+    applications: [ "Supabase" ],
+    repository: "https://github.com/RDev00/konfia"
+  }
+]
---
+++ src/content/skills.ts
@@ -0,0 +1,66 @@
+export const skills = [
+  {
+    name: "JavaScript",
+    img: "https://cdn.iconscout.com/icon/free/png-256/free-javascript-logo-icon-svg-download-png-2284965.png?f=webp&w=128"
+  },
+  {
+    name: "TypeScript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
+  },
+  {
+    name: "Dart",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png"
+  },
+  {
+    name: "MongoDB",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2ZYtHv2OLXmthRPbkmENZRXuqBVDwlsrZ1A&s"
+  },
+  {
+    name: "Postgre SQL",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1280px-Postgresql_elephant.svg.png"
+  },
+  {
+    name: "Git",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/3840px-Git_icon.svg.png"
+  },
+  {
+    name: "React",
+    img: "https://react.dev/images/brand/logo_dark.svg",
+    isFramework: true
+  },
+  {
+    name: "Express JS",
+    img: "https://expressjs.com/images/favicon.png",
+    isFramework: true
+  },
+  {
+    name: "NextJS",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV9uzErWz9EXqZDxZ5lP9aYpMz8eK6rr5X3w&s",
+    isFramework: true
+  },
+  {
+    name: "Astro",
+    img: "https://astro.build/favicon.svg",
+    isFramework: true
+  },
+  {
+    name: "GitHub",
+    img: "https://github.githubassets.com/favicons/favicon-dark.svg",
+    isApplication: true
+  },
+  {
+    name: "NeonSQL",
+    img: "https://neon.com/favicon/favicon.svg",
+    isApplication: true
+  },
+  {
+    name: "Vercel",
+    img: "https://cdn.brandfetch.io/vercel.com/fallback/lettermark/theme/dark/h/256/w/256/icon?c=1bfwsmEH20zzEfSNTed",
+    isApplication: true
+  },
+  {
+    name: "Supabase",
+    img: "https://supabase.com/favicon/favicon-196x196.png",
+    isApplication: true
+  },
+]
